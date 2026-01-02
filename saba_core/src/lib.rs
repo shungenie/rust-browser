621b1f406f8e60b3cd389a8aba1fa04cf2d19ecc
shungenie <ca02291@cartahd.com>
2026-01-02 16:28:35 +0900

    ✨ BrowserおよびPage構造体を実装し、HTMLレスポンスを処理する機能を追加しました

--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -1,8 +1,10 @@
 #![no_std]
 
- extern crate alloc;
+extern crate alloc;
 
- pub mod error;
- pub mod http;
- pub mod renderer;
- pub mod url;
+pub mod browser;
+pub mod error;
+pub mod http;
+pub mod renderer;
+pub mod url; 
+pub mod utils;
