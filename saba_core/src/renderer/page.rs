dd6c586035dd9f0a77f05c81fb7f8ac72b8586ee
shungenie <ca02291@cartahd.com>
2026-01-11 21:50:45 +0900

    ✨ Page構造体にスタイルとレイアウトビューを追加し、受信したレスポンスを処理するメソッドを修正しました

--- saba_core/src/renderer/page.rs
+++ saba_core/src/renderer/page.rs
@@ -1,19 +1,27 @@
-use crate::alloc::string::ToString;
 use crate::browser::Browser;
 use crate::http::HttpResponse;
 use crate::renderer::dom::node::Window;
 use crate::renderer::html::parser::HtmlParser;
 use crate::renderer::html::token::HtmlTokenizer;
-use crate::utils::convert_dom_to_string;
+use crate::display_item::DisplayItem;
+use crate::renderer::css::cssom::StyleSheet;
+use crate::renderer::layout::layout_view::LayoutView;
+use crate::renderer::css::cssom::CssParser;
+use crate::renderer::css::token::CssTokenizer;
+use crate::renderer::dom::api::get_style_content;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
 use alloc::string::String;
 use core::cell::RefCell;
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone)]
 pub struct Page {
     browser: Weak<RefCell<Browser>>,
     frame: Option<Rc<RefCell<Window>>>,
+    style: Option<StyleSheet>,
+    layout_view: Option<LayoutView>,
+    display_items: Vec<DisplayItem>,
 }
 
 impl Page {
@@ -21,6 +29,9 @@ impl Page {
         Self {
             browser: Weak::new(),
             frame: None,
+            style: None,
+            layout_view: None,
+            display_items: Vec::new(),
         }
     }
 
@@ -28,22 +39,53 @@ impl Page {
         self.browser = browser;
     }
 
-    pub fn receive_response(&mut self, response: HttpResponse) -> String {
+    fn set_layout_view(&mut self) {
+        let dom = match &self.frame {
+            Some(frame) => frame.borrow().document(),
+            None => return,
+        };
+
+        let style = match self.style.clone() {
+            Some(style) => style,
+            None => return,
+        };
+
+        let layout_view = LayoutView::new(dom, &style);
+        self.layout_view = Some(layout_view);
+    }
+
+    fn paint_tree(&mut self) {
+        if let Some(layout_view) = &self.layout_view {
+            self.display_items = layout_view.paint();
+        }
+    }
+
+    pub fn receive_response(&mut self, response: HttpResponse) {
         self.create_frame(response.body());
 
-        // デバッグ用にDOMツリーを文字列化して返す
-        if let Some(frame) = &self.frame {
-            let dom = frame.borrow().document().clone();
-            let debug = convert_dom_to_string(&Some(dom));
-            return debug;
-        }
+        self.set_layout_view();
 
-        "".to_string()
+        self.paint_tree();
     }
 
     fn create_frame(&mut self, html: String) {
         let html_tokenizer = HtmlTokenizer::new(html);
         let frame = HtmlParser::new(html_tokenizer).construct_tree();
+        let dom = frame.borrow().document();
+
+        let style = get_style_content(dom);
+        let css_tokenizer = CssTokenizer::new(style);
+        let cssom = CssParser::new(css_tokenizer).parse_stylesheet();
+
         self.frame = Some(frame);
+        self.style = Some(cssom);
+    }
+
+    pub fn display_items(&self) -> Vec<DisplayItem> {
+        self.display_items.clone()
+    }
+
+    pub fn clear_display_items(&mut self) {
+        self.display_items = Vec::new();
     }
 }
