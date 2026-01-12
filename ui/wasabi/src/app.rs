9a51c658203cb7838f5044c844ada8fb09b27797
shungenie <ca02291@cartahd.com>
2026-01-12 15:00:05 +0900

    ✨ UIの更新処理を追加し、描画機能を改善した

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,17 +1,17 @@
 use crate::alloc::string::ToString;
+use crate::cursor::Cursor;
 use alloc::format;
 use alloc::rc::Rc;
 use alloc::string::String;
 use core::cell::RefCell;
-use crate::cursor::Cursor;
 use noli::error::Result as OsResult;
 use noli::prelude::SystemApi;
 use noli::println;
+use noli::rect::Rect;
 use noli::sys::api::MouseEvent;
 use noli::sys::wasabi::Api;
 use noli::window::StringSize;
 use noli::window::Window;
-use noli::rect::Rect;
 use saba_core::browser::Browser;
 use saba_core::constants::WHITE;
 use saba_core::constants::WINDOW_HEIGHT;
@@ -19,8 +19,11 @@ use saba_core::constants::WINDOW_INIT_X_POS;
 use saba_core::constants::WINDOW_INIT_Y_POS;
 use saba_core::constants::WINDOW_WIDTH;
 use saba_core::constants::*;
+use saba_core::display_item::DisplayItem;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
+use saba_core::renderer::layout::computed_style::FontSize;
+use saba_core::renderer::layout::computed_style::TextDecoration;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum InputMode {
@@ -104,7 +107,10 @@ impl WasabiUI {
         Ok(())
     }
 
-    pub fn start(&mut self, handle_url: fn(String) -> Result<HttpResponse, Error>) -> Result<(), Error> {
+    pub fn start(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         self.setup()?;
 
         self.run_app(handle_url)?;
@@ -112,18 +118,25 @@ impl WasabiUI {
         Ok(())
     }
 
-    fn run_app(&mut self, handle_url: fn(String) -> Result<HttpResponse, Error>) -> Result<(), Error> {
+    fn run_app(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         loop {
             self.handle_mouse_input(handle_url)?;
             self.handle_key_input(handle_url)?;
         }
     }
 
-    fn handle_mouse_input(&mut self, handle_url: fn(String) -> Result<HttpResponse, Error>) -> Result<(), Error> {
+    fn handle_mouse_input(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         if let Some(MouseEvent {
             button: button,
             position,
-        }) = Api::get_mouse_cursor_info() {
+        }) = Api::get_mouse_cursor_info()
+        {
             self.window.flush_area(self.cursor.rect());
             self.cursor.set_position(position.x, position.y);
             self.window.flush_area(self.cursor.rect());
@@ -137,13 +150,19 @@ impl WasabiUI {
                 );
 
                 // ウィンドウの外をクリックされた時はなにもしない
-                if relative_pos.0 < 0 || relative_pos.0 > WINDOW_WIDTH || relative_pos.1 < 0 || relative_pos.1 > WINDOW_HEIGHT {
+                if relative_pos.0 < 0
+                    || relative_pos.0 > WINDOW_WIDTH
+                    || relative_pos.1 < 0
+                    || relative_pos.1 > WINDOW_HEIGHT
+                {
                     println!("button clicked OUTSIDE window: {button:?} at position: {position:?}");
                     return Ok(());
                 }
 
                 // ツールバーの範囲をクリックされたとき、InputModeをEditingに変更する
-                if relative_pos.1 < TOOLBAR_HEIGHT + TITLE_BAR_HEIGHT && relative_pos.1 >= TITLE_BAR_HEIGHT {
+                if relative_pos.1 < TOOLBAR_HEIGHT + TITLE_BAR_HEIGHT
+                    && relative_pos.1 >= TITLE_BAR_HEIGHT
+                {
                     self.clear_address_bar()?;
                     self.input_url = String::new();
                     self.input_mode = InputMode::Editing;
@@ -158,7 +177,10 @@ impl WasabiUI {
         Ok(())
     }
 
-    fn handle_key_input(&mut self, handle_url: fn(String) -> Result<HttpResponse, Error>) -> Result<(), Error> {
+    fn handle_key_input(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         match self.input_mode {
             InputMode::Normal => {
                 // InputModeがNormalの時、キー入力を無視
@@ -189,14 +211,22 @@ impl WasabiUI {
 
     fn update_address_bar(&mut self) -> Result<(), Error> {
         // アドレスバーを白く塗りつぶす
-        if self.window.fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2).is_err() {
+        if self
+            .window
+            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2)
+            .is_err()
+        {
             return Err(Error::InvalidUI(
                 "failed to fill address bar rect".to_string(),
             ));
         }
 
         // input_urlをアドレスバーに描画する
-        if self.window.draw_string(BLACK, 74, 6, &self.input_url, StringSize::Medium, false).is_err() {
+        if self
+            .window
+            .draw_string(BLACK, 74, 6, &self.input_url, StringSize::Medium, false)
+            .is_err()
+        {
             return Err(Error::InvalidUI(
                 "failed to draw string to address bar".to_string(),
             ));
@@ -208,7 +238,7 @@ impl WasabiUI {
                 WINDOW_INIT_X_POS,
                 WINDOW_INIT_Y_POS + TITLE_BAR_HEIGHT,
                 WINDOW_WIDTH,
-                TOOLBAR_HEIGHT
+                TOOLBAR_HEIGHT,
             )
             .expect("failed to create a rect for the address bar"),
         );
@@ -220,7 +250,9 @@ impl WasabiUI {
         // アドレスバーを白く塗りつぶす
         if self
             .window
-            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2).is_err() {
+            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2)
+            .is_err()
+        {
             return Err(Error::InvalidUI(
                 "failed to fill address bar rect".to_string(),
             ));
@@ -257,20 +289,96 @@ impl WasabiUI {
             }
         }
 
+        self.update_ui()?;
+
         Ok(())
     }
 
     fn clear_content_area(&mut self) -> Result<(), Error> {
         // コンテンツエリアを白く塗りつぶす
-        if self.window.fill_rect(WHITE, 0, TOOLBAR_HEIGHT + 2, CONTENT_AREA_WIDTH, CONTENT_AREA_HEIGHT - 2)
-            .is_err() {
-                return Err(Error::InvalidUI(
-                    "failed to clear content area".to_string(),
-                ));
-            }
+        if self
+            .window
+            .fill_rect(
+                WHITE,
+                0,
+                TOOLBAR_HEIGHT + 2,
+                CONTENT_AREA_WIDTH,
+                CONTENT_AREA_HEIGHT - 2,
+            )
+            .is_err()
+        {
+            return Err(Error::InvalidUI("failed to clear content area".to_string()));
+        }
 
-            self.window.flush();
+        self.window.flush();
 
         Ok(())
+    }
+
+    fn update_ui(&mut self) -> Result<(), Error> {
+        let display_items = self
+            .browser
+            .borrow()
+            .current_page()
+            .borrow()
+            .display_items();
+
+        for item in display_items {
+            match item {
+                DisplayItem::Text {
+                    text,
+                    style,
+                    layout_point,
+                } => {
+                    if self
+                        .window
+                        .draw_string(
+                            style.color().code_u32(),
+                            layout_point.x() + WINDOW_PADDING,
+                            layout_point.y() + WINDOW_PADDING + TOOLBAR_HEIGHT,
+                            &text,
+                            convert_font_size(style.font_size()),
+                            style.text_decoration() == TextDecoration::Underline,
+                        )
+                        .is_err()
+                    {
+                        return Err(Error::InvalidUI(
+                            "failed to draw text display item".to_string(),
+                        ));
+                    }
+                }
+                DisplayItem::Rect {
+                    style,
+                    layout_point,
+                    layout_size,
+                } => {
+                    if self
+                        .window
+                        .fill_rect(
+                            style.background_color().code_u32(),
+                            layout_point.x() + WINDOW_PADDING,
+                            layout_point.y() + WINDOW_PADDING + TOOLBAR_HEIGHT,
+                            layout_size.width(),
+                            layout_size.height(),
+                        )
+                        .is_err()
+                    {
+                        return Err(Error::InvalidUI("failed to draw a string".to_string()));
+                    }
+                }
+            }
+        }
+
+        self.window.flush();
+
+        Ok(())
+    }
+
+    fn convert_font_size(size: FontSize) -> StringSize {
+        match size {
+            FontSize::Medium => StringSize::Medium,
+            FontSize::XLarge => StringSize::Large,
+            FontSize::XXLarge => StringSize::XLarge,
+        }
     }
 }
