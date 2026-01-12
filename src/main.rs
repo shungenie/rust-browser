bcd7f93352cb8ddab2ccf5523ee93fb12ffdf9a2
shungenie <ca02291@cartahd.com>
2026-01-12 12:51:26 +0900

    ✨ ui_wasabiパッケージを追加し、WasabiUI構造体を実装しました

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,27 @@
 
 extern crate alloc;
 
-use crate::alloc::string::ToString;
+use alloc::rc::Rc;
+use core::cell::RefCell;
 use noli::*;
 use saba_core::browser::Browser;
-use saba_core::http::HttpResponse;
-
-static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
-Data: xx xx xx
-
-
-<html>
-<head></head>
-<body>
-    <h1 id="title">H1 title</h1>
-    <h2 class="class">H2 title</h2>
-    <p>Test text.</p>
-    <p>
-        <a href="example.com">Link1</a>
-        <a href="example.com">Link2</a>
-    </p>
-</body>
-</html>
-"#;
+use ui_wasabi::app::WasabiUI;
 
 fn main() -> u64 {
+    // Browser構造体を初期化
     let browser = Browser::new();
 
-    let response =
-        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
-    let page = browser.borrow().current_page();
-    page.borrow_mut().receive_response(response);
+    // WasabiUI構造体を初期化
+    let ui = Rc::new(RefCell::new(WasabiUI::new(browser)));
+
+    // アプリの実行を開始
+    match ui.borrow_mut().start() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("browser fails to start {:?}", e);
+            return 1;
+        }
+    };
 
     0
 }
---
+++ ui/wasabi/src/app.rs
@@ -0,0 +1,86 @@
+use crate::alloc::string::ToString;
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::window::Window;
+use saba_core::browser::Browser;
+use saba_core::constants::WHITE;
+use saba_core::constants::WINDOW_HEIGHT;
+use saba_core::constants::WINDOW_INIT_X_POS;
+use saba_core::constants::WINDOW_INIT_Y_POS;
+use saba_core::constants::WINDOW_WIDTH;
+use noli::error::Result as OsResult;
+use noli::window::StringSize;
+use saba_core::constants::*;
+use alloc::format;
+use saba_core::error::Error;
+
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+
+    fn setup_toolbar(&mut self) -> OsResult<()> {
+        // ツールバーの背景の資格を描画
+        self.window.fill_rect(LIGHTGREY, 0, 0, WINDOW_WIDTH, TOOLBAR_HEIGHT)?;
+
+        // ツールバーとコンテンツエリアの境目の線を描画
+        self.window
+            .draw_line(GREY, 0, TOOLBAR_HEIGHT, WINDOW_WIDTH - 1, TOOLBAR_HEIGHT)?;
+        self.window.draw_line(DARKGREY, 0, TOOLBAR_HEIGHT + 1, WINDOW_WIDTH - 1, TOOLBAR_HEIGHT + 1)?;
+
+        // アドレスバーの横に"Address:"と表示
+        self.window.draw_string(BLACK, 5, 5, "Address:", StringSize::Medium, false)?;
+
+        // アドレスバーの四角を描画
+        self.window.fill_rect(WHITE, 70, 2, WINDOW_WIDTH - 74, 2 + ADDRESSBAR_HEIGHT)?;
+
+        // アドレスバーの影の線を描画
+        self.window.draw_line(GREY, 70, 2, WINDOW_WIDTH - 4, 2)?;
+        self.window.draw_line(GREY, 70, 2, 70, 2 + ADDRESSBAR_HEIGHT)?;
+        self.window.draw_line(BLACK, 71, 3, WINDOW_WIDTH - 5, 3)?;
+
+        self.window.draw_line(GREY, 71, 3, 71, 1 + ADDRESSBAR_HEIGHT)?;
+
+        Ok(())
+    }
+
+    fn setup(&mut self) -> Result<(), Error> {
+        if let Err(error) = self.setup_toolbar() {
+            // OsResult と Resultが持つエラー型が異なるため、変換する
+            return Err(Error::InvalidUI(format!("failed to initialize a toolbar with error: {:#?}", error)));
+        }
+        // 画面を更新する
+        self.window.flush();
+        Ok(())
+    }
+
+    pub fn start(&mut self) -> Result<(), Error> {
+        self.setup()?;
+
+        self.run_app()?;
+
+        Ok(())
+    }
+
+    fn run_app(&mut self) -> Result<(), Error> {
+        // 後ほど実装
+        Ok(())
+    }
+}
