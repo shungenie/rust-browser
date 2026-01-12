f08ba8ab75c28dce091f83a276fe7859c2c5e05a
shungenie <ca02291@cartahd.com>
2026-01-12 13:26:32 +0900

    ✨ キー入力処理を追加し、アプリの実行ループでハンドリングを行った

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -99,6 +99,7 @@ impl WasabiUI {
     fn run_app(&mut self) -> Result<(), Error> {
         loop {
             self.handle_mouse_input()?;
+            self.handle_key_input()?;
         }
     }
 
@@ -111,6 +112,14 @@ impl WasabiUI {
             if button.l() || button.c() || button.r() {
                 println!("mouse button clicked {:?}", button);
             }
+        }
+
+        Ok(())
+    }
+
+    fn handle_key_input(&mut self) -> Result<(), Error> {
+        if let Some(c) = Api::read_key() {
+            println!("input text {:?}", c);
         }
 
         Ok(())
