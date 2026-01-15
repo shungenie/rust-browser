4b9d8a9e9e04bf8c1a855c540cbb29350aa547ec
shungenie <ca02291@cartahd.com>
2026-01-15 22:00:02 +0900

    ✨ JsLexer構造体を追加し、トークンのイテレータを実装した

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -1,7 +1,56 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
     /// https://262.ecma-international.org/#sec-punctuators
     Punctuator(char),
     /// https://262.ecma-international.org/#sec-literals-numeric-literals
     Number(u64),
+}
+
+pub struct JsLexer {
+    pos: usize, // どこまで読み込んだか
+    input: Vec<char>,
+}
+
+impl JsLexer {
+    pub fn new(js: String) -> Self {
+        Self {
+            pos: 0,
+            input: js.chars().collect(),
+        }
+    }
+}
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // ホワイトスペースまたは改行文字が続く限り次の位置に進める
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }    
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.'  => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            _ => unimplemented!("char {:?} is not supported yet", c)
+        };
+
+        Some(token)
+    }
 }
