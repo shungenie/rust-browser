25335c7a7e7cb1da375eb5cc17726e30560c9db6
shungenie <ca02291@cartahd.com>
2026-09-18 22:44:40 +0900

    add test

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -88,4 +88,30 @@ mod tests {
         let mut lexer = JsLexer::new(input).peekable();
         assert!(lexer.peek().is_none());
     }
+
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(42)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i].clone()), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(1), Token::Punctuator('+'), Token::Number(2)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i].clone()), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
 }
