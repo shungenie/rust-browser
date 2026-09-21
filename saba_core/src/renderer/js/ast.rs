f659d86b8f95fdd2c7107a02528179f6a371fc36
shungenie <ca02291@cartahd.com>
2026-09-21 22:48:05 +0900

    define Node

---
+++ saba_core/src/renderer/js/ast.rs
@@ -0,0 +1,27 @@
+use alloc::rc::Rc;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Node{
+    ExpressionStatement(Option<Rc<Node>>),
+    AdditiveExpression {
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    },
+    AssignmentExpression {
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    },
+    MemberExpression {
+        object: Option<Rc<Node>>,
+        property: Option<Rc<Node>>,
+    },
+    NumericLiteral(u64),
+}
+
+impl Node {
+    pub fn new_expression_statement(expression: Option<Rc<Self>>) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::ExpressionStatement(expression)))
+    }
+}
